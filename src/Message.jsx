ea6e1570939787931a11bd18cfd9f55743e131d8
match8885 <match.tenkoh68@gmail.com>
2025-11-01 09:54:42 +0900

    Add files via upload

---
+++ src/Message.jsx
@@ -0,0 +1,25 @@
+import { motion } from "framer-motion";
+
+export default function Message() {
+  return (
+    <motion.div
+      className="
+        mt-14 text-center max-w-[85%]
+        text-orange-900
+        leading-relaxed
+      "
+      initial={{ opacity: 0, y: 15 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1.8, ease: "easeOut" }}
+    >
+      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold mb-4">
+        瑞希ちゃん いつもありがとう！
+      </h1>
+
+      <p className="text-lg sm:text-lg md:text-xl lg:text-2xl opacity-80">
+        一緒にいろんな場所へ行けて楽しかった！<br />
+        これからも一緒にたくさん思い出を作っていこうね😊
+      </p>
+    </motion.div>
+  );
+}
